8dc687c40a1b5bc310f84297583fc3ca287ada26
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2018-12-19 16:54:59 +0530

    Rename decque.c to stacks and queues/decque.c

--- stacks and queues/decque.c
+++ stacks and queues/decque.c
@@ -1,5 +1,5 @@
 /*
-Dequeue. A double-ended queue or deque (pronounced ìdeckî) is a generalization of a stack and a queue that supports adding and removing items from either the front or the back of the data structure. Create a generic data type Deque that implements the following API:
+Dequeue. A double-ended queue or deque (pronounced ‚Äúdeck‚Äù) is a generalization of a stack and a queue that supports adding and removing items from either the front or the back of the data structure. Create a generic data type Deque that implements the following API:
 
 public class Deque<Item> implements Iterable<Item> {
    public Deque()                           // construct an empty deque
