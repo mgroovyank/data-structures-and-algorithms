3bb9409bd328d6b9d1a98063993bc8de47a1c1e3
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2019-03-09 09:57:11 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+//Implement Hash Code for Strings:Horner's Method
+//@author mgroovyank(MAYANK CHHIPA)
+
+#include <stdio.h>
+int main() {
+  char s[]={'c','a','l','l'};
+  int i;
+  int hash=0;
+  for(i=0;i<4;i++){
+    hash=s[i]+31*hash;
+  }
+  printf("%d",hash);
+}
