cdd0e6a93698c15fdf8879765789df9c0e3a7a70
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2019-05-30 21:20:10 +0530

    Update selection sort.c

--- elementary sorts/selection sort.c
+++ elementary sorts/selection sort.c
@@ -3,6 +3,8 @@ int main(){
   float a[]={1.0,5.0,7.0,4.12,2.34},t,min=a[0];
   int i,j;
   int length_a=sizeof(a)/sizeof(a[0]);
+  
+  // code that does sorting
   for(i=0;i<length_a;i++){
     for(j=i+1;j<length_a;j++){
       if(a[j]<=a[i]){
@@ -15,6 +17,8 @@ int main(){
   for(i=0;i<length_a;i++){
     printf(" %f ",a[i]);
   }
+  
+  // code to check if selection sort worked or not
   for(i=0;i<length_a-1;i++){
     if(a[i]<=a[i+1]){
       continue;
